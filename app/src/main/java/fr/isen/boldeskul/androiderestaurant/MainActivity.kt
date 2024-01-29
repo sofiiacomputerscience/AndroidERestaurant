6609bc5c2b74595250192585c0641bdbe84cbbe7
Sofiia Boldeskul <sofiia.computerscience@gmail.com>
2024-01-29 11:05:51 +0100

    Buttons View

--- app/src/main/java/fr/isen/boldeskul/androiderestaurant/MainActivity.kt
+++ app/src/main/java/fr/isen/boldeskul/androiderestaurant/MainActivity.kt
@@ -3,14 +3,20 @@ package fr.isen.boldeskul.androiderestaurant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import fr.isen.boldeskul.androiderestaurant.ui.theme.AndroidERestaurantTheme
+import androidx.compose.ui.res.painterResource
+import androidx.compose.material3.Button
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,26 +24,52 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidERestaurantTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     Greeting("Android")
+
                 }
+                SetupView()
+                GreetingPreview()
             }
         }
     }
-}
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Column {
+                Text(
+                text = "Hello $name!",
+                modifier = modifier
+                )
+            Text(
+                    text = "Hello test",
+                    modifier = modifier
+                )
+        }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidERestaurantTheme {
-        Greeting("Android")
+    }
+    @Preview
+    @Composable
+    fun SetupView(){
+        Column{
+            Image(painterResource(R.drawable.ic_launcher_foreground), null)
+             Button(onClick = { /*TODO*/ }){
+                 Text(stringResource(R.string.menu_starters))
+             }
+             Button(onClick = { /*TODO*/ }){
+                 Text(stringResource(R.string.menu_main))
+             }
+             Button(onClick = { /*TODO*/ }){
+                    Text(stringResource(R.string.menu_dessert))
+                }
+        }
+    }
+    @Composable
+    fun GreetingPreview() {
+        AndroidERestaurantTheme {
+            Greeting("Android")
+        }
     }
 }
