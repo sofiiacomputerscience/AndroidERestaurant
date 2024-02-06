50152b98b513b085abd5bf50b835e54c22b8fca6
Sofiia Boldeskul <sofiia.computerscience@gmail.com>
2024-02-06 16:00:35 +0100

    Almost finished TP design

--- app/src/main/java/fr/isen/boldeskul/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/boldeskul/androiderestaurant/HomeActivity.kt
@@ -21,6 +21,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.material3.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
@@ -29,10 +31,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import fr.isen.boldeskul.androiderestaurant.ui.theme.ralewayFontFamily
+import java.util.Locale
 
 enum class DishType{
     STARTER, MAIN, DESSERT
@@ -60,8 +64,43 @@ class HomeActivity : ComponentActivity(), MenuInterface {
                     Scaffold(
                         topBar = { MexicanRestaurantTopApp() },
                     ) {
-                        SetupView(this)
+                        paddingValues ->
+                        Box(modifier = Modifier
+                            .fillMaxSize()
+                            .padding(PaddingValues())) {
+                            SetupView(this@HomeActivity)
+
+
+                            Row(
+                                modifier = Modifier
+                                    .align(Alignment.TopEnd)
+                                    .padding(top = paddingValues.calculateTopPadding() + 16.dp,
+                                             end = 16.dp,
+                                             start = 16.dp),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.mexicanguy),
+                                    contentDescription = "Decorative Image",
+                                    modifier = Modifier.size(150.dp) // Adjust the size accordingly
+                                )
+                                Text(
+                                    text = "¡Bienvenido a nuestro restaurante!",
+                                    style = MaterialTheme.typography.bodyLarge.copy(
+                                        color = Color(0xFFC62828),
+                                        fontWeight = FontWeight.Bold,
+                                        fontFamily = ralewayFontFamily,
+                                        fontSize = 25.sp
+                                    ),
+                                    modifier = Modifier
+                                        .padding(start = 20.dp)
+
+
+                                )
+                            }
+                        }
                     }
+
                 }
             }
         }
@@ -102,37 +141,59 @@ fun GreetingPreview() {
         SetupView(HomeActivity())
     }
 }
-@Composable
-fun SetupView(menu: MenuInterface){
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Column(
 
+
+@Composable
+fun TextStyledButton(text: String, onClick: () -> Unit){
+    TextButton(
+        onClick = onClick,
+        modifier = Modifier
+            .height(50.dp) // Adjust the height as needed
+            .fillMaxWidth() // Make the button fill the width of its parent
+    ) {
+        Text(
+            text = text.toUpperCase(Locale.ROOT), // Set the text to uppercase
+            color = Color(0xFFFB8C00),
+            style = TextStyle(
+                fontWeight = FontWeight.Bold, // Use bold text
+                fontSize = 18.sp, // Keep the font size as is
+                fontFamily = ralewayFontFamily // Your custom font family
+            )
+        )
+    }
+    Divider(
+        color = Color(0xFF039BE5), // Adjust the divider color to match your theme
+        thickness = 1.dp, // Adjust the thickness as needed
+        modifier = Modifier.padding(horizontal = 50.dp) // Add horizontal padding
+    )
+}
+
+
+@Composable
+fun SetupView(menu: MenuInterface) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(20.dp)
         )
         {
 //        Image(painterResource(R.drawable.ic_launcher_foreground), null)
+            TextStyledButton(text = stringResource(R.string.menu_starters)) {
+                menu.dishPressed(DishType.STARTER)
+            }
 
-            Button(onClick = { menu.dishPressed(DishType.STARTER) }, modifier = Modifier
-                .fillMaxWidth(0.8f)
-                .height(50.dp),
-                shape = MaterialTheme.shapes.small) {
-                Text(text = stringResource(R.string.menu_starters),
-                    fontSize = 18.sp,
-                    fontFamily = ralewayFontFamily)
+            TextStyledButton(text = stringResource(R.string.menu_main)) {
+                menu.dishPressed(DishType.MAIN)
             }
-            Button(onClick = { menu.dishPressed(DishType.MAIN) }) {
-                Text(stringResource(R.string.menu_main),
-                    fontSize = 18.sp,
-                    fontFamily = ralewayFontFamily)
-            }
-            Button(onClick = { menu.dishPressed(DishType.DESSERT)}) {
-                Text(stringResource(R.string.menu_dessert),
-                    fontSize = 18.sp,
-                    fontFamily = ralewayFontFamily)
+            TextStyledButton(text = stringResource(R.string.menu_dessert)) {
+                menu.dishPressed(DishType.DESSERT)
             }
         }
     }
 }
+
 
 
