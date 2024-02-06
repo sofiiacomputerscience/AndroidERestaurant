787e477313f818195570c9c9aef9f2f66f35621f
Sofiia Boldeskul <sofiia.computerscience@gmail.com>
2024-02-06 12:13:50 +0100

    Changed buttons design + added font + added top app

--- app/src/main/java/fr/isen/boldeskul/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/boldeskul/androiderestaurant/HomeActivity.kt
@@ -1,9 +1,11 @@
 package fr.isen.boldeskul.androiderestaurant
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,6 +19,20 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.material3.Button
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+import androidx.compose.material3.Scaffold
+
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import fr.isen.boldeskul.androiderestaurant.ui.theme.ralewayFontFamily
 
 enum class DishType{
     STARTER, MAIN, DESSERT
@@ -29,6 +45,8 @@ interface MenuInterface{
 
 
 class HomeActivity : ComponentActivity(), MenuInterface {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -37,8 +55,13 @@ class HomeActivity : ComponentActivity(), MenuInterface {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {
-                SetupView(this)
+                )
+                {
+                    Scaffold(
+                        topBar = { MexicanRestaurantTopApp() },
+                    ) {
+                        SetupView(this)
+                    }
                 }
             }
         }
@@ -49,22 +72,29 @@ class HomeActivity : ComponentActivity(), MenuInterface {
     }
 }
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SetupView(menu: MenuInterface){
-    Column{
-        Image(painterResource(R.drawable.ic_launcher_foreground), null)
-        Button(onClick = { menu.dishPressed(DishType.STARTER)}){
-            Text(stringResource(R.string.menu_starters))
-        }
-        Button(onClick = { /*TODO*/ }){
-            Text(stringResource(R.string.menu_main))
-        }
-        Button(onClick = { /*TODO*/ }){
-            Text(stringResource(R.string.menu_dessert))
-        }
-    }
+fun MexicanRestaurantTopApp(){
+    SmallTopAppBar(
+        title = {
+
+            Text(
+                text = "Mexican Restaurant",
+                color = Color.White,
+                style = TextStyle(
+                    fontSize = 24.sp, // Adjust the font size as needed
+                    fontFamily = ralewayFontFamily, // Apply your custom font family
+                    fontWeight = FontWeight.Bold, // Apply bold style if desired
+                    letterSpacing = 0.5.sp // Adjust letter spacing if needed
+                )
+            )
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(
+            containerColor = Color(0xFF039BE5)
+        )
+    )
 }
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
@@ -72,3 +102,37 @@ fun GreetingPreview() {
         SetupView(HomeActivity())
     }
 }
+@Composable
+fun SetupView(menu: MenuInterface){
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Column(
+
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        )
+        {
+//        Image(painterResource(R.drawable.ic_launcher_foreground), null)
+
+            Button(onClick = { menu.dishPressed(DishType.STARTER) }, modifier = Modifier
+                .fillMaxWidth(0.8f)
+                .height(50.dp),
+                shape = MaterialTheme.shapes.small) {
+                Text(text = stringResource(R.string.menu_starters),
+                    fontSize = 18.sp,
+                    fontFamily = ralewayFontFamily)
+            }
+            Button(onClick = { menu.dishPressed(DishType.MAIN) }) {
+                Text(stringResource(R.string.menu_main),
+                    fontSize = 18.sp,
+                    fontFamily = ralewayFontFamily)
+            }
+            Button(onClick = { menu.dishPressed(DishType.DESSERT)}) {
+                Text(stringResource(R.string.menu_dessert),
+                    fontSize = 18.sp,
+                    fontFamily = ralewayFontFamily)
+            }
+        }
+    }
+}
+
+
--- app/src/main/java/fr/isen/boldeskul/androiderestaurant/ui/theme/Theme.kt
+++ app/src/main/java/fr/isen/boldeskul/androiderestaurant/ui/theme/Theme.kt
@@ -14,36 +14,37 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import fr.isen.boldeskul.androiderestaurant.R
 
 private val DarkColorScheme = darkColorScheme(
-        primary = Purple80,
-        secondary = PurpleGrey80,
-        tertiary = Pink80
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-        primary = Purple40,
-        secondary = PurpleGrey40,
-        tertiary = Pink40
-
-        /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
+)
+val ralewayFontFamily = FontFamily(
+    Font(R.font.raleway_medium) // Replace with the actual font file name
 )
 
 @Composable
 fun AndroidERestaurantTheme(
-        darkTheme: Boolean = isSystemInDarkTheme(),
-        // Dynamic color is available on Android 12+
-        dynamicColor: Boolean = true,
-        content: @Composable () -> Unit
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
 ) {
+    // Define your custom font family
+    val RalewayFontFamily = FontFamily(
+        Font(R.font.raleway_medium) // Replace with the actual font file name
+    )
+
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
@@ -63,8 +64,8 @@ fun AndroidERestaurantTheme(
     }
 
     MaterialTheme(
-            colorScheme = colorScheme,
-            typography = Typography,
-            content = content
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
     )
 }
